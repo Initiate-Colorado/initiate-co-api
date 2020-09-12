1ea6290178b7e83f08c5effe0032e80084d3027f
Danny Ramos <danramos21@gmail.com>
2020-09-12 15:12:37 -0600

    Added ballot due dates to scraper Co-authored-by: Rostam <rostammahabadi@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from sos_scraper import webScraper
+from co_ga_scraper import coGaScraper
 
 
 # initializes api
@@ -8,6 +9,7 @@ app = Flask(__name__)
 api = Api(app)
 
 BALLOTS = webScraper()
+DETAILS = coGaScraper()
 
 parser = reqparse.RequestParser()
 
@@ -16,8 +18,13 @@ class BallotDetails(Resource):
   def get(self):
     return BALLOTS
 
+class BallotDueDates(Resource):
+  def get(self):
+    return DETAILS
+
 
 api.add_resource(BallotDetails, '/ballots/')
+api.add_resource(BallotDueDates, '/ballots/duedates/')
 
 
 if __name__ == '__main__':
