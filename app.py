094b88ddb5f1d59af559a7edb548e87846ed9083
Danny Ramos <danramos21@gmail.com>
2020-09-12 12:57:04 -0600

    updated app file to fit our needs

--- app.py
+++ app.py
@@ -1,34 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from sos_scraper import webScraper
 
 # initializes api
 app = Flask(__name__)
 api = Api(app)
 
-STUDENTS = {
-  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
-  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
-  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
-  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
-}
+BALLOTS = webScraper()
 
 parser = reqparse.RequestParser()
 
-class StudentsList(Resource):
+class BallotDetails(Resource):
   def get(self):
-    return STUDENTS
+    return BALLOTS
 
 
-class Student(Resource):
-  def get(self, student_id):
-    if student_id not in STUDENTS:
-      return 'Not found', 404
-    else:
-      return STUDENTS[student_id]
+api.add_resource(BallotDetails, '/ballots/')
 
-
-api.add_resource(StudentsList, '/students/')
-api.add_resource(Student, '/students/<student_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
