44fe220011d3f1a045c3b837cbbe72eee84c3f05
Danny Ramos <danramos21@gmail.com>
2020-09-15 12:25:01 -0600

    complete rework of scraper

--- co_ga_scraper.py
+++ co_ga_scraper.py
@@ -1,9 +1,8 @@
+
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import re
-
 def coGaScraper():
-  results = {}
   url = "https://leg.colorado.gov/content/how-file-initiatives"
   response = Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
   web_page = urlopen(response).read()
@@ -11,21 +10,21 @@ def coGaScraper():
   section = page_soup.find('div', 'field-item even')
   titles = section.findAll('h5', text=True)
   list_of_due_dates = section.findAll('p')[1:]
+  results = []
   for title in titles:
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[0].contents[0].get_text())).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[1].contents[0].contents[0].contents[0].replace(u'\xa0', ' '))).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[3].contents[0].get_text())).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[4].contents[0].get_text())).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[5].contents[0].get_text())).group(0)
-    results["step_due_date"] = ''
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[6].contents[0].get_text())).group(0)
-    results["step_due_date"] = ''
-    results["step_due_date"] = 'Number of valid signatures required for the 2020 election:  124,632'
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[8].contents[0].get_text())).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[9].contents[0].get_text())).group(0)
-    results["step_due_date"] = re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[10].contents[0].get_text())).group(0)
-    results["step_due_date"] = ''
-    results["step_due_date"] = ''
-  return results
-  
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[0].contents[0].get_text())).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[1].contents[0].contents[0].contents[0].replace(u'\xa0', ' '))).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[3].contents[0].get_text())).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[4].contents[0].get_text())).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[5].contents[0].get_text())).group(0))
+    results.append('')
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[6].contents[0].get_text())).group(0))
+    results.append('')
+    results.append('Number of valid signatures required for the 2020 election:  124,632')
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[8].contents[0].get_text())).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[9].contents[0].get_text())).group(0))
+    results.append(re.search('(\w{3,}\s\d{1,},\s\d{1,4})',str(list_of_due_dates[10].contents[0].get_text())).group(0))
+    results.append('')
+    results.append('')
+    return results
   
